fa37b2ebebcab4a0a9152e77d8a7c86d58690ac5
Kkura-chan <thienkiempro1@gmail.com>
2022-10-28 11:18:00 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require("dotenv").config();
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+viewEngine(app);
+initWebRoutes(app);
+
+const PORT = process.env.PORT || 3069;
+app.listen(PORT, (req, res) => {
+  console.log("App is listening on port: ", PORT);
+});
